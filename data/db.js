8a28134982401cac4970c90007e04c6d6184bbb8
Walterhart <walterwhart1@gmail.com>
2023-05-30 13:41:37 -0400

    Setup files and display tweets

---
+++ data/db.js
@@ -0,0 +1,58 @@
+export const tweetsData = [   
+    {
+        handle: `@DefNotAHyena 💎`,
+        profilePic: `../assets/images/DefNotAHyena.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Buy Breadcoin, ETH Make 🫓 🫓 🫓 low low prices. 
+            Guaranteed return on investment. HMU DMs open!!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@BlueJay ✅`,
+        profilePic: `../assets/images/BlueJay.jpg`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
+        replies: [
+                  {
+                handle: `@Eagle ✅`,
+                profilePic: `images/Eagle.png`,
+                tweetText: `Yes! Sign me up! 😎🛩`,
+            },
+                  {
+                handle: `@Cassowary  ✅`,
+                profilePic: `../assets/images/Cassowary.jpg`,
+                tweetText: `I went last year😴`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@Kakapo`,
+        profilePic: `../assets/images/Kakapo.jpg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Are you a coder if you only know HTML?`,
+        replies: [
+            {
+                handle: `@Pigeon ☣️`,
+                profilePic: `../assets/images/Pigeons.jpg`,
+                tweetText: `No. Onviosuly not. Go get a job in BcDonald's.`,
+            },
+            {
+                handle: `@Penguins`,
+                profilePic: `../assets/images/Penguins.jpg`,
+                tweetText: `You are wonderful just as you are! ❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]

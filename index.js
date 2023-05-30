53090e153c76aafe406dcc6fc79ca6943862a61b
Walterhart <walterwhart1@gmail.com>
2023-05-30 17:16:20 -0400

    Add retweet functionality and change hear and icon color base clicked

--- index.js
+++ index.js
@@ -1,46 +1,78 @@
-import { tweetsData } from './data/db.js';
+import { tweetsData } from "./data/db.js";
 
 // Selecting tweet input and tweet button elements
-const tweetInput = document.getElementById('tweet-input');
-const tweetBtn = document.getElementById('tweet-btn');
+const tweetInput = document.getElementById("tweet-input");
+const tweetBtn = document.getElementById("tweet-btn");
 
 // Event listener for the tweet button click
-tweetBtn.addEventListener('click', function(){
-    console.log(tweetInput.value); // Logging the value of the tweet input
+tweetBtn.addEventListener("click", function () {
+  console.log(tweetInput.value); // Logging the value of the tweet input
 });
 
 // Event listener for click events on the document
-document.addEventListener('click', function(e){
-    if(e.target.dataset.like){
-       handleLikeClick(e.target.dataset.like); // Calling handleLikeClick function if the clicked element has a 'data-like' attribute
-    }
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like); // Calling handleLikeClick function if the clicked element has a 'data-like' attribute
+  }
+  else if(e.target.dataset.retweet){
+    handleRetweetClick(e.target.dataset.retweet)
+}
 });
 
 // Function to handle the like click
-function handleLikeClick(tweetId){ 
-    // Finding the target tweet object based on the tweet ID
-    const targetTweetObj = tweetsData.filter(function(tweet){
-        return tweet.uuid === tweetId;
-    })[0];
-    
-    // Updating the like count and isLiked property of the target tweet
-    if (targetTweetObj.isLiked){
-        targetTweetObj.likes--;
-    }
-    else{
-        targetTweetObj.likes++;
-    }
-    targetTweetObj.isLiked = !targetTweetObj.isLiked;
-    render(); // Rendering the updated tweet feed
+function handleLikeClick(tweetId) {
+  // Finding the target tweet object based on the tweet ID
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+
+  // Updating the like count and isLiked property of the target tweet
+  if (targetTweetObj.isLiked) {
+    targetTweetObj.likes--;
+  } else {
+    targetTweetObj.likes++;
+  }
+  targetTweetObj.isLiked = !targetTweetObj.isLiked;
+  render(); // Rendering the updated tweet feed
 }
 
-// Function to generate HTML for the tweet feed
-function getFeedHtml(){
-    let feedHtml = ``;
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
     
-    // Iterating over tweetsData array and constructing HTML markup for each tweet
-    tweetsData.forEach(function(tweet){
-        feedHtml += `
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render() 
+}
+
+
+// Function to generate HTML for the tweet feed
+function getFeedHtml() {
+  let feedHtml = ``;
+  
+
+  // Iterating over tweetsData array and constructing HTML markup for each tweet
+  tweetsData.forEach(function (tweet) {
+    let likeIconClass = ''
+        
+    if (tweet.isLiked){
+        likeIconClass = 'liked'
+    }
+    
+    let retweetIconClass = ''
+    
+    if (tweet.isRetweeted){
+        retweetIconClass = 'retweeted'
+    }
+
+    feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
         <img src="${tweet.profilePic}" class="profile-pic">
@@ -55,13 +87,13 @@ function getFeedHtml(){
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-heart"
+                    <i class="fa-solid fa-heart ${likeIconClass}"
                     data-like="${tweet.uuid}"
                     ></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet"
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
                     data-retweet="${tweet.uuid}"
                     ></i>
                     ${tweet.retweets}
@@ -69,15 +101,14 @@ function getFeedHtml(){
             </div>   
         </div>            
     </div>
-</div>
-`;
-   });
-   return feedHtml;
+</div>`;
+  });
+  return feedHtml;
 }
 
 // Function to render the tweet feed
-function render(){
-    document.getElementById('feed').innerHTML = getFeedHtml();
+function render() {
+  document.getElementById("feed").innerHTML = getFeedHtml();
 }
 
 render(); // Initial rendering of the tweet feed
